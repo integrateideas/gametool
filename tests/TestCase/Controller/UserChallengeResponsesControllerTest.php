1c70d0c82c866cf9f5548920e2a376e37a0ff386
Vishakha24 <vishakha.chaudhary@twinspark.co>
2017-09-01 11:58:36 +0530

    user challenge response

---
+++ tests/TestCase/Controller/UserChallengeResponsesControllerTest.php
@@ -0,0 +1,76 @@
+<?php
+namespace App\Test\TestCase\Controller;
+
+use App\Controller\UserChallengeResponsesController;
+use Cake\TestSuite\IntegrationTestCase;
+
+/**
+ * App\Controller\UserChallengeResponsesController Test Case
+ */
+class UserChallengeResponsesControllerTest extends IntegrationTestCase
+{
+
+    /**
+     * Fixtures
+     *
+     * @var array
+     */
+    public $fixtures = [
+        'app.user_challenge_responses',
+        'app.users',
+        'app.roles',
+        'app.social_profiles',
+        'app.challenges',
+        'app.challenge_types'
+    ];
+
+    /**
+     * Test index method
+     *
+     * @return void
+     */
+    public function testIndex()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test view method
+     *
+     * @return void
+     */
+    public function testView()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test add method
+     *
+     * @return void
+     */
+    public function testAdd()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test edit method
+     *
+     * @return void
+     */
+    public function testEdit()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test delete method
+     *
+     * @return void
+     */
+    public function testDelete()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+}
